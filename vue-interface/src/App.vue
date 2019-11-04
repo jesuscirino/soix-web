75f3b445fca83fd85c4ee48e210230b068a17253
jesuscirino <jesuscirino@gmail.com>
2019-11-04 02:14:48 -0600

    1. Cleaning and setup main.js and App.vue

--- vue-interface/src/App.vue
+++ vue-interface/src/App.vue
@@ -1,24 +1,23 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="main-app" class="container">
+    <h4>{{title}}</h4>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
+  name: "MainApp",
+  data: function() {
+    return {
+      title: "Apoint List"
+    };
   }
-}
+};
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
